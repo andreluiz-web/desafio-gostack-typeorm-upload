8e37f3c66948971087007acfd522c395e0ab8c73
andreluiz-web <andreluizdefreitas0@gmail.com>
2020-11-02 18:10:12 -0300

    save-files

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<Transaction> {
+    const transactionRepo = getCustomRepository(TransactionsRepository);
+
+    const findedId = await transactionRepo.findOne(id);
+
+    if (findedId === undefined) {
+      throw new AppError('transaction ID does not exists');
+    }
+
+    const deletedTransaction = await transactionRepo.remove(findedId);
+
+    return deletedTransaction;
   }
 }
 
